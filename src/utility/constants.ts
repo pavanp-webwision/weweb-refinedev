1909cecf8a01413cb81e80770da5bd0d4aa069c9
dev4 webwision <pavan.p@webwision.com>
2024-08-20 17:00:09 +0530

    add env

--- src/utility/constants.ts
+++ src/utility/constants.ts
@@ -1,2 +1,2 @@
-export const API_URL = process.env.NEXT_PUBLIC_TOKEN_URL;
-export const TOKEN_KEY = process.env.NEXT_PUBLIC_API_URL;
+export const API_URL = process.env.NEXT_PUBLIC_API_URL;
+export const TOKEN_KEY = process.env.NEXT_PUBLIC_TOKEN_URL;
