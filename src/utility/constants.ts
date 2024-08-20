de96e5fd0a37f5a5bad078e375b3c179b96dd00a
dev4 webwision <pavan.p@webwision.com>
2024-08-20 16:55:07 +0530

    add env

--- src/utility/constants.ts
+++ src/utility/constants.ts
@@ -1,3 +1,2 @@
-export const API_URL =
-  'https://3b74-2405-201-d-6145-b5f1-9e1a-1f76-6086.ngrok-free.app';
-export const TOKEN_KEY = 'strapi-jwt-token';
+export const API_URL = process.env.NEXT_PUBLIC_TOKEN_URL;
+export const TOKEN_KEY = process.env.NEXT_PUBLIC_API_URL;
